b0fe90458d7933a0884c0cbccada4c64fa03a2b5
karapeprajakta <karapeprajakta1@gmail.com>
2023-10-02 04:28:20 -0700

    'updates'

---
+++ server.js
@@ -0,0 +1,8 @@
+var express=require('express');
+var app=express();
+app.use(express.static('public'));
+app.get("/",(req,res)=>{
+res.send("Welcome to IET");
+});
+app.listen(8000);
+console.log("App is listening on port 8000");
